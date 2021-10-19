bcf7592f5015b4a4846f2ff93d1ab45ac5dba453
david-djordjevic <david.djordjevic@enjoying.rs>
2021-10-20 00:38:34 +0200

    introducing the form

--- src/components/openingHoursCard.js
+++ src/components/openingHoursCard.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import {Col, Row} from 'react-bootstrap';
+import {Col, Row, Container} from 'react-bootstrap';
 
 const OpeningHoursCard = (props) => {
 	const {openingHours} = props;
 	return (
 		<React.Fragment>
 			<h2>Opening hours</h2>
-			{Object.keys(openingHours).map(function (daySequence, daySequenceIndex) {
-				return (
-					<React.Fragment>
-						<Col key={daySequence} xs={6}>
-							<Row key={daySequenceIndex}>{daySequence}</Row>
-						</Col>
-						<Col key={daySequenceIndex} xs={12}>
-							{openingHours[daySequence].map((shift, index) => {
-								return <Row key={index}>{shift}</Row>;
-							})}
-						</Col>
-					</React.Fragment>
-				);
-			})}
+			<Col>
+				{Object.keys(openingHours).map(function (daySequence, daySequenceIndex) {
+					return (
+						<React.Fragment>
+							<Container>
+								<Row>
+									<Col key={daySequence} xs={6}>
+										<Row key={daySequenceIndex}>{daySequence}</Row>
+									</Col>
+									<Col key={daySequenceIndex} xs={6}>
+										{openingHours[daySequence].map((shift, index) => {
+											return <Row key={index}>{shift}</Row>;
+										})}
+									</Col>
+									&nbsp; &nbsp; &nbsp;
+								</Row>
+							</Container>
+						</React.Fragment>
+					);
+				})}
+			</Col>
 		</React.Fragment>
 	);
 };
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Header from '../components/header';
 import RestaurantDetails from '../components/restaurantDetails';
 import {getRestaurant, getOpeningHours} from '../service/restaurantService';
-import {Button, Col} from 'react-bootstrap';
+import {Button, Col, Form} from 'react-bootstrap';
 import OpeningHoursCard from '../components/openingHoursCard';
 
 const Dashboard = () => {
@@ -10,7 +10,12 @@ const Dashboard = () => {
 	const [addressLine, setAddressLine] = useState('');
 	const [showOpeningHours, setShowOpeningHours] = useState(false);
 	const [openingHours, setOpeningHours] = useState('');
-	const restaurantId = 'GXvPAor1ifNfpF0U5PTG0w';
+	const [restaurantId, setRestaurantId] = useState('');
+	const [restaurantLoaded, setRestaurantLoaded] = useState(false);
+
+	// ohGSnJtMIC5nPfYRi_HTAg
+
+	// GXvPAor1ifNfpF0U5PTG0w
 
 	const fetchAndShowOpeningHours = () => {
 		if (!openingHours) {
@@ -21,18 +26,50 @@ const Dashboard = () => {
 		setShowOpeningHours(!showOpeningHours);
 	};
 
-	useEffect(() => {
+	const fetchRestaurantDetails = () => {
+		getRestaurant(
+			restaurantId,
+			(response) => {
+				setRestaurantName(response.name);
+				setAddressLine(response.addressLine);
+				setRestaurantLoaded(true);
+			},
+			() => {
+				setRestaurantLoaded(false);
+			}
+		);
+	};
+
+	/*useEffect(() => {
 		getRestaurant(restaurantId, (response) => {
 			setRestaurantName(response.name);
 			setAddressLine(response.addressLine);
 		});
-	}, []);
+	}, []);*/
 
 	return (
 		<React.Fragment>
 			<Header title="Restaurant management UI" />
-			<RestaurantDetails restaurantName={restaurantName} addressLine={addressLine} />
-			<Button onClick={() => fetchAndShowOpeningHours(restaurantId)}>Show opening hours</Button>
+
+			<Form.Group className="m-0">
+				<Form.Control
+					rows="3"
+					placeholder="Enter restaurant id"
+					value={restaurantId}
+					onChange={(e) => setRestaurantId(e.target.value)}
+					type="text"
+				/>
+				<Button className="btnFormSend" variant="outline-success" onClick={() => fetchRestaurantDetails(restaurantId)}>
+					Search
+				</Button>
+			</Form.Group>
+
+			{restaurantLoaded ? (
+				<React.Fragment>
+					<RestaurantDetails restaurantName={restaurantName} addressLine={addressLine} />
+					<Button onClick={() => fetchAndShowOpeningHours(restaurantId)}>Show opening hours</Button>
+				</React.Fragment>
+			) : null}
 			{showOpeningHours ? <OpeningHoursCard openingHours={openingHours} /> : null}
 		</React.Fragment>
 	);
